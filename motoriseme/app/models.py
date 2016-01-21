1f990221f04612947b812872d5ff3afcc0992683
staticbg <staticbg@gmail.com>
2016-01-21 20:55:03 +0200

    adds custom user - Rider

--- motoriseme/app/models.py
+++ motoriseme/app/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Rider(models.Model):
+	user = models.OneToOneField(User, primary_key=True)
+	first_name = models.CharField(max_length=30, null=True)
+	nickname = models.CharField(max_length=30, blank=True)
+	last_name = models.CharField(max_length=30, blank=True)
--- motoriseme/app/views.py
+++ motoriseme/app/views.py
@@ -2,28 +2,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from app.models import Rider
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+def register_rider(user, first_name, nickname, last_name):
+    rider = Rider(user, first_name, nickname, last_name)
+    rider.save()
 
 
 def register(request):
     print('RECEIVED REQUEST: ' + request.method)
     notifications = []
     if request.method == 'GET':
-      if request.user.is_authenticated():
-          notifications.append("Вече сте логнат.")
-          return redirect('/')
-      return render(request, 'register.html', {})
+        if request.user.is_authenticated():
+            notifications.append("Вече сте логнат.")
+            return redirect('/')
+        return render(request, 'register.html', {})
     else:
-      if request.user.is_authenticated():
-        notifications.append("Вече сте логнат.")
+        if request.user.is_authenticated():
+            notifications.append("Вече сте логнат.")
+            return render(request, 'index.html', {'messages': notifications})
+        user = User.objects.create_user(request.POST['username'],
+                                        request.POST['email'],
+                                        request.POST['password'])
+        register_rider(user.id,
+                       request.POST['first_name'],
+                       request.POST['nickname'],
+                       request.POST['last_name'])
+        notifications.append("Регистрира се. Животът е хубав.")
         return render(request, 'index.html', {'messages': notifications})
-      user = User.objects.create_user(request.POST['username'],
-                                      request.POST['email'],
-                                      request.POST['password'])
-      notifications.append("Регистрира се. Животът е хубав.")
-      return render(request, 'index.html', {'messages': notifications})
+
 
 def user_login(request):
     username = request.POST['username']
@@ -39,6 +50,7 @@ def user_login(request):
     else:
         notifications.append("Невалидно име или парола.")
         return render(request, 'index.html', {'messages': notifications})
+
 
 def user_logout(request):
     logout(request)
