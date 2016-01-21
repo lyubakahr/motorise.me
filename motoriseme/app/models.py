7eeb844751affd0d3913d0e8f6cabce3b12025b0
toshle <rosen_ra4ev@abv.bg>
2016-01-21 19:54:50 +0200

    Add event model

--- motoriseme/app/models.py
+++ motoriseme/app/models.py
@@ -1,1 +1,21 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
+
+class Event(models.Model):
+    name = models.CharField(max_length = 100)
+    date = models.DateTimeField(default = datetime.now())
+    start_point = models.CharField(max_length = 254)
+    start_point_coordinates = models.CharField(max_length = 100)
+    end_point = models.CharField(max_length = 254)
+    end_point_coordinates = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 254)
+    noob_friendly = models.BooleanField(default = False)
+    creator = models.ForeignKey(User)
+
+    @classmethod
+    def get_all(cls):
+        return cls.objects.all()
+
+    def __str__(self):
+        return '{}'.format(self.name)
--- motoriseme/app/views.py
+++ motoriseme/app/views.py
@@ -2,28 +2,48 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from app.models import Event
 
 def index(request):
     return render(request, 'index.html', {})
 
+def create_event(request):
+    print('RECEIVED REQUEST: ' + request.method)
+    notifications = []
+    if request.method == 'POST':
+        if request.user.is_authenticated():
+            event = Event(name = request.POST['name'],
+                          date = request.POST['date'],
+                          start_point = request.POST['start_point'],
+                          start_point_coordinates = request.POST['start_point_coordinates'],
+                          end_point = request.POST['end_point'],
+                          end_point_coordinates = request.POST['end_point_coordinates'],
+                          description = request.POST['description'],
+                          noob_friendly = request.POST['noob_friendly'],
+                          creator = request.user)
+            event.save()
+            notifications.append("Честит сбор.")
+        return render(request, 'index.html', {'messages': notifications})
+    else:
+        return render(request, 'index.html', {'messages': notifications})
 
 def register(request):
     print('RECEIVED REQUEST: ' + request.method)
     notifications = []
     if request.method == 'GET':
-      if request.user.is_authenticated():
-          notifications.append("Вече сте логнат.")
-          return redirect('/')
-      return render(request, 'register.html', {})
+        if request.user.is_authenticated():
+            notifications.append("Вече сте логнат.")
+            return redirect('/')
+        return render(request, 'register.html', {})
     else:
-      if request.user.is_authenticated():
-        notifications.append("Вече сте логнат.")
+        if request.user.is_authenticated():
+            notifications.append("Вече сте логнат.")
+            return render(request, 'index.html', {'messages': notifications})
+        user = User.objects.create_user(request.POST['username'],
+                                        request.POST['email'],
+                                        request.POST['password'])
+        notifications.append("Регистрира се. Животът е хубав.")
         return render(request, 'index.html', {'messages': notifications})
-      user = User.objects.create_user(request.POST['username'],
-                                      request.POST['email'],
-                                      request.POST['password'])
-      notifications.append("Регистрира се. Животът е хубав.")
-      return render(request, 'index.html', {'messages': notifications})
 
 def user_login(request):
     username = request.POST['username']
