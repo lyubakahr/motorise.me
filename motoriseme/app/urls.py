7d9acc4c63d9e2b99d25a3fd0481bea45b0bcc99
lyubakahr <lyubo.hristov@gmail.com>
2016-01-25 17:11:15 +0200

    "update user" service

--- motoriseme/app/urls.py
+++ motoriseme/app/urls.py
@@ -4,17 +4,17 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^register', views.register, name='register'),
-    url(r'^login', views.user_login, name='login'),
-    url(r'^logout', views.user_logout, name='logout'),
-    url(r'^create_event', views.create_event, name='create_event'),
-    url(r'^delete_event', views.delete_event, name='delete_event'),
-
-    # REST urls start here
-    url(r'^user_info', views.get_user_info, name='user_info'),
-    url(r'^user_events', views.get_user_events, name='user_events'),
-    url(r'^all_events', views.get_all_events, name='all_events'),
-    url(r'^event_info', views.get_event_info, name='event_info'),
-    url(r'^all_user_comments', views.get_all_user_comments, name='all_user_comments'),
-    url(r'^all_event_comments', views.get_all_event_comments, name='all_event_comments')
-]
+    url(r'^register$', views.register, name='register'),
+    url(r'^login$', views.user_login, name='login'),
+    url(r'^logout$', views.user_logout, name='logout'),
+    url(r'^create_event$', views.create_event, name='create_event'),
+     # need to check DELETE method ...
+    url(r'^delete_event$', views.delete_event, name='delete_event'),
+    url(r'^user_info$', views.get_user_info, name='user_info'),
+    url(r'^user_events$', views.get_user_events, name='user_events'),
+    url(r'^all_events$', views.get_all_events, name='all_events'),
+    url(r'^event_info$', views.get_event_info, name='event_info'),
+    url(r'^all_user_comments$', views.get_all_user_comments, name='all_user_comments'),
+    url(r'^all_event_comments$', views.get_all_event_comments, name='all_event_comments'),
+    url(r'^update_rider$', views.update_rider, name='update_rider'),
+ ]
