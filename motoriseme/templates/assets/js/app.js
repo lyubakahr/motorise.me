f41fde1905001270275164e54f415374907ea51d
toshle <rosen_ra4ev@abv.bg>
2016-01-26 19:57:24 +0200

    Add home page

--- motoriseme/templates/assets/js/app.js
+++ motoriseme/templates/assets/js/app.js
@@ -1,1 +1,47 @@
 $(document).foundation();
+
+window.addEventListener("load", function () {
+  if(document.getElementById("signup-button") != null) {
+    document.getElementById("signup-button").addEventListener("click", function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      var form = document.getElementById("register-form");
+      form.submit();
+    });
+  }
+  
+  L.mapbox.accessToken = 'pk.eyJ1IjoidG9zaGxlIiwiYSI6ImNpanR4dzIxODAwMGx0em00eDNwb2c1dnEifQ.0AEcgIpeNUpMCQc5HvKr6A';
+  var map = L.mapbox.map('map', 'mapbox.streets');
+
+  var myLayer = L.mapbox.featureLayer().addTo(map);
+
+  if (!navigator.geolocation) {
+    alert('Geolocation is not available');
+  } else {
+    map.locate();
+  }
+
+  map.on('locationfound', function(e) {
+    map.fitBounds(e.bounds);
+
+    myLayer.setGeoJSON({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [e.latlng.lng, e.latlng.lat]
+      },
+      properties: {
+        'title': 'Here I am!',
+        'marker-color': '#ff8888',
+        'marker-symbol': 'star'
+      }
+    });
+
+  });
+
+  map.on('locationerror', function(e) {
+    console.log(e);
+  });
+
+
+});
